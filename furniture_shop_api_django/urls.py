5ed6169ee05eea57d8ca895632db745c00466e2e
Joseph Hayfron Acquah <joeacq81@gmail.com>
2024-12-14 09:43:04 +0000

    Remove settings.py

--- furniture_shop_api_django/urls.py
+++ furniture_shop_api_django/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin1/", admin.site.urls),
     path("api/v1/users/", include("users.urls")),
     path("api/v1/", include("products.urls")),
 ]
